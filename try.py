ef75e12a8ad5d8f1f04d13eaee09eba8f5c728e0
iqmal-1916447 <iqmal.jamaludin@gmail.com>
2022-07-25 15:44:45 +0800

    Adding functionality for real time tracking in Google Earth using .kml format

--- try.py
+++ try.py
@@ -1,17 +1,18 @@
 # try to parse GPS NMEA data file
-nmea_data = "GPS200525.nmea"
+nmea_data = "GPS200526.nmea"
 
 def read_NMEA():
     # Read the NMEA data file
     infile = open(nmea_data, 'r')
 
-    # Storing longitude, latitude and altitude
+    # Array to store position & altitude
     latitude = []
     longitude = []
     altitude = []
 
     # Read NMEA line by line
     try:
+        counter = 1     # Counting input data from sample file
         for i in infile:
             dataGPS = i.split(',')
             
@@ -27,7 +28,7 @@ def read_NMEA():
                     # Convert latitude coordinate ddmm.mmmm to decimal degree (dd)
                     latitude_degree = int(latitude_GPS/100)     # divide by 100 to move decimal, then truncate decimal places; so get 2 most significant digit
                     latitude_minute = latitude_GPS - latitude_degree * 100  # get the minute part
-                    latitude = latitude_degree + latitude_minute / 60
+                    latitude = latitude_degree + latitude_minute / 60 
 
                     # Get NMEA longitude
                     longitude_GPS = float(dataGPS[5])
@@ -37,34 +38,50 @@ def read_NMEA():
                     # Convert longitude coordinate dddmm.mmmm to decimal degree (dd)
                     longitude_degree = int(longitude_GPS/100)
                     longitude_minute = longitude_GPS - longitude_degree * 100
-                    longitude = longitude_degree + longitude_minute / 60
+                    longitude = longitude_degree + longitude_minute / 60 
 
-                    # print(f"Latitude: {latitude:.6f}; Longitude: {longitude:.6f}")
 
             # Get Altitude from GGA message
             if dataGPS[0] == "$GPGGA":
                 if dataGPS[6] != "0":   # Valid position fix
                     if dataGPS[6] == "1":
-                        print("GPS fix (SPS)")
+                        print(str(counter) + ". GPS fix (SPS)")
                     elif dataGPS[6] == "2":
-                        print("DGPS fix")
+                        print(str(counter) + ". DGPS fix")
                     elif dataGPS[6] == "3":
-                        print("PPS fix")
+                        print(str(counter) + ". PPS fix")
 
                     altitude = dataGPS[9]
-                    # print (f"Altitude: {altitude}\n\n")
+                    counter += 1
 
-            print(f"Latitude: {latitude:.6f}; Longitude: {longitude:.6f}; Altitude: {altitude}") 
+                    print(f"Latitude: {latitude:.6f}; Longitude: {longitude:.6f}; Altitude: {altitude}\n") 
 
-
-
-
-
-
-
+            # Display the position & altitude in KML format (view in Google Earth)
+            # Output / write to KML file
+            with open("position.kml", 'w') as position:
+                position.write("""<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2">
+    <Document>
+        <Placemark>
+        <name>
+                Rider's Location [Longitude: %s  Latitude: %s] 
+        </name>
+        <description>
+            Real-time position via NMEA data
+        </description>
+        <Point>
+            <coordinates>%s, %s</coordinates>
+        </Point>
+        </Placemark>
+    </Document>
+</kml>
+""" %(longitude, latitude, longitude, latitude))
 
     except Exception as e:
         print(e)
+
+    # Close NMEA data file
+    infile.close()
 
 if __name__ == "__main__":
     # Run the function
